30de15c0676431f9c8d075faf7d564873ad5d35d
sharanh17 <sharanhidaduggi17@gmail.com>
2024-05-16 22:35:29 +0530

    editdone

--- src/components/Edit/EditUserForm.js
+++ src/components/Edit/EditUserForm.js
@@ -1,6 +1,7 @@
 // EditUserForm.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './EditUserForm.css';
 
 const EditUserForm = ({ user, updateUser, setEditingUser }) => {
   const [currentUser, setCurrentUser] = useState(user);
@@ -27,20 +28,22 @@ const EditUserForm = ({ user, updateUser, setEditingUser }) => {
   };
 
   return (
-    <div>
+    <div className="modal">
       {error && <p>Error: {error}</p>}
-      <form onSubmit={handleSubmit}>
-        <label>First Name:</label>
-        <input type="text" name="firstName" value={currentUser.firstName} onChange={handleInputChange} />
-        <label>Last Name:</label>
-        <input type="text" name="lastName" value={currentUser.lastName} onChange={handleInputChange} />
-        <label>Email:</label>
-        <input type="email" name="email" value={currentUser.email} onChange={handleInputChange} />
-        <label>Department:</label>
-        <input type="text" name="department" value={currentUser.department} onChange={handleInputChange} />
-        <button type="submit">Update User</button>
-        <button onClick={() => setEditingUser(null)}>Cancel</button>
-      </form>
+      <div className="modal-content">
+        <form onSubmit={handleSubmit}>
+          <label>First Name:</label>
+          <input type="text" name="firstName" value={currentUser.firstName} onChange={handleInputChange} />
+          <label>Last Name:</label>
+          <input type="text" name="lastName" value={currentUser.lastName} onChange={handleInputChange} />
+          <label>Email:</label>
+          <input type="email" name="email" value={currentUser.email} onChange={handleInputChange} />
+          <label>Department:</label>
+          <input type="text" name="department" value={currentUser.department} onChange={handleInputChange} />
+          <button type="submit">Update User</button>
+          <button onClick={() => setEditingUser(null)}>Cancel</button>
+        </form>
+      </div>
     </div>
   );
 };
